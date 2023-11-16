2f99b6153d46552a95c84c45af23cdbc739e7aec
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-11-16 15:10:20 -0500

    Call /intensity route

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,20 @@
+import { useEffect } from "react";
+
 export default function App() {
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await fetch(
+          "https://api.carbonintensity.org.uk/intensity"
+        );
+        const intensity = await response.json();
+        console.log(intensity.data[0]);
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, []);
+
   return (
     <>
       <div></div>
