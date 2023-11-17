ac935882d466ac74243583f30515a54f3d0644c6
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-11-16 20:03:12 -0500

    Get intensity data for all periods in current day

--- src/Chart.jsx
+++ src/Chart.jsx
@@ -1,3 +1,42 @@
+import { useEffect, useState } from "react";
+
 export default function Chart() {
-  return "<Chart />";
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const date = new Date();
+
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+
+    let currentDate = `${year}-${month}-${day}`;
+
+    const BASE_URL = `https://api.carbonintensity.org.uk/intensity/date/${currentDate}/`;
+    const urls = [];
+    for (let i = 1; i <= 48; i++) {
+      urls.push(BASE_URL + i);
+    }
+
+    (async () => {
+      await Promise.all(
+        urls.map(async (url) => {
+          const response = await fetch(url);
+          const d = await response.json();
+          console.log(d);
+          setData((currData) => {
+            return [...currData, d.data[0].intensity.forecast];
+          });
+        })
+      );
+    })();
+  }, []);
+
+  return (
+    <>
+      {data.map((value) => {
+        return <div>{value}</div>;
+      })}
+    </>
+  );
 }
